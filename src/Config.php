b06ff8305a4b1876d870476d16eada93771761fc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-04 13:40:36 +0200

    Create Config.php

---
+++ src/Config.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bermuda\Config;
+
+
+use Laminas\ConfigAggregator\ArrayProvider;
+use Laminas\ConfigAggregator\ConfigAggregator;
+
+
+/**
+ * Class Config
+ * @package Bermuda\Config
+ */
+final class Config
+{
+    public function __construct()
+    {
+        throw new \RuntimeException(__CLASS__ . ' is not instantiable!');
+    }
+
+    public static bool $useCache = false;
+    public static ?string $cacheFile = null;
+
+    /**
+     * @param callable ...$providers
+     * @return array
+     */
+    public static function merge(callable ... $providers): array
+    {
+        $providers[] = static function()
+        {
+            return [ConfigAggregator::ENABLE_CACHE => self::$useCache]);
+        };
+        
+        return (new ConfigAggregator($providers, self::$cacheFile))->getMergedConfig();
+    }
+}
