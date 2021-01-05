122f97d880e8af9b16ab14b8757092a3a4154034
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-05 20:50:17 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -29,7 +29,7 @@ final class Config
     {
         $providers[] = static function()
         {
-            return [ConfigAggregator::ENABLE_CACHE => !self::devMode];
+            return ['debug' => !self::$devMode, ConfigAggregator::ENABLE_CACHE => !self::$devMode,];
         };
         
         return (new ConfigAggregator($providers, self::$cacheFile))->getMergedConfig();
