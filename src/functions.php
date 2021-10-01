2a4609a6aa8a3cd5d7bf4af817726f4e2eaef208
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-01 18:52:00 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -2,14 +2,20 @@
 
 namespace Bermuda\Config;
 
+use Psr\Container\ContainerInterface;
+
 /**
- * @param string $entry
- * @param null $default
- * @return AppInterface|mixed|string|null
+ * @param ContainerInterface $container
+ * @param string $id
+ * @param mixed $default
+ * @param bool $invokable
+ * @return mixed
  */
 function cget(ContainerInterface $container, string $id, $default = null, bool $invokable = false): mixed
 {
     if ($container->has($id)) {
-    
+        return $container->get($id);
     }
+    
+    return $invokable && is_callable($default) ? $default($container) : $default ; 
 }
