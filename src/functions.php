9fcf35d7f900c3bd0df50052d8b92363bdb11c47
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-23 16:25:31 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -9,14 +9,15 @@ use Psr\Container\ContainerInterface;
  * @param string $id
  * @param mixed $default
  * @param bool $invokable
+ * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
  * @throws ContainerExceptionInterface Error while retrieving the entry.
  * @return mixed
  */
 function cget(ContainerInterface $container, string $id, $default = null, bool $invokable = true): mixed
 {
-    try {
+    if ($container->has($id)) {
         return $container->get($id);
-    } catch (NotFoundExceptionInterface) {
-        return $invokable && is_callable($default) ? $default($container) : $default ;
     }
+    
+    return $invokable && is_callable($default) ? $default($container) : $default ; 
 }
