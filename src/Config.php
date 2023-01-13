33fbdf15e0b69ca82b8b12310bd7664f6634aa2f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-13 15:53:20 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -9,6 +9,9 @@ final class Config
     public static bool $devMode = true;
     public static ?string $cacheFile = null;
     
+    public const app_timezone = 'app.timezone';
+    public const app_debug_mode_enable = 'app.debug';
+    
     /**
      * @param callable ...$providers
      * @return array
@@ -19,7 +22,7 @@ final class Config
             array_merge($providers, 
                 [
                     static fn(): array => [
-                        'debug' => self::$devMode,
+                        self::app_debug_mode_enable => self::$devMode,
                         ConfigAggregator::ENABLE_CACHE => !self::$devMode && self::$cacheFile != null
                     ]
                 ]
