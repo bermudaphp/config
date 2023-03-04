88c4ac222df4f077bdf939a4069a45819c1412ad
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-04 17:09:03 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,23 +1,37 @@
 <?php
 
-namespace Bermuda\Config;
+namespace Bermuda\Config {
+    use Psr\Container\ContainerInterface;
 
-use Psr\Container\ContainerInterface;
+    /**
+     * @param ContainerInterface $container
+     * @param string $id
+     * @param mixed $default
+     * @param bool $invokable
+     * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
+     * @throws ContainerExceptionInterface Error while retrieving the entry.
+     * @return mixed
+     */
+    function cget(ContainerInterface $container, string $id, $default = null, bool $invokable = true): mixed
+    {
+        if ($container->has($id)) {
+            return $container->get($id);
+        }
 
-/**
- * @param ContainerInterface $container
- * @param string $id
- * @param mixed $default
- * @param bool $invokable
- * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
- * @throws ContainerExceptionInterface Error while retrieving the entry.
- * @return mixed
- */
-function cget(ContainerInterface $container, string $id, $default = null, bool $invokable = true): mixed
-{
-    if ($container->has($id)) {
-        return $container->get($id);
+        return $invokable && is_callable($default) ? $default($container) : $default ;
     }
-    
-    return $invokable && is_callable($default) ? $default($container) : $default ; 
+}
+
+namespace Bermuda\Stdlib {
+    if (!function_exists('to_array')) {
+        function to_array(iterable|object $arrayable): array
+        {
+            if ($arrayable instanceof Arrayable) return $arrayable->toArray();
+            if ($arrayable instanceof \IteratorAggregate) return \iterator_to_array($arrayable->getIterator());
+            if ($arrayable instanceof \Iterator) return \iterator_to_array($arrayable);
+            if (is_array($arrayable)) return $arrayable;
+
+            return \get_object_vars($arrayable);
+        }
+    }
 }
