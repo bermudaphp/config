0e79b1565fe5a06dd6bc0da3d4ad2f5edc121373
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-08 17:44:06 +0200

    Update PhpFileProvider.php

--- src/PhpFileProvider.php
+++ src/PhpFileProvider.php
@@ -2,25 +2,40 @@
 
 namespace Bermuda\Config;
 
-use \Laminas\ConfigAggregator\PhpFileProvider as LaminasFileProvider;
+use Bermuda\Flysystem\Location;
+use Laminas\ConfigAggregator\GlobTrait;
 
-class PhpFileProvider
+final class PhpFileProvider
 {
-    private LaminasFileProvider $provider;
+    use GlobTrait;
+    private $normalizer;
 
-    public function __construct(string $pattern)
-    {
-        $this->provider = new LaminasFileProvider($pattern);
+    public function __construct(
+        private readonly string $pattern,
+        private readonly bool $filenameAsKey = true,
+        callable $normalizer = null
+    ) {
+        $this->normalizer = $normalizer;
     }
 
     public function __invoke(): array
     {
         $cfg = [];
-        foreach (($this->provider)() as $item) {
-            if (empty($item) || !is_array($item)) continue ;
-            $cfg = array_merge_recursive($cfg, $item);
+        foreach ($this->glob($this->pattern) as $file) {
+            if (str_ends_with($basename = basename($file), '.php')) {
+                if (empty($data = include $file) || !is_array($data)) continue ;
+                if ($this->filenameAsKey) $data = [$this->normalize($basename) => $data];
+                
+                $cfg = array_merge_recursive($cfg, $data);
+            }
         }
 
         return $cfg;
+    }
+
+    private function normalize(string $basename): string
+    {
+        if ($this->normalizer) return ($this->normalizer)($basename);
+        else return str_replace(['.', 'php', 'local', 'development'], '', $basename);
     }
 }
