d5e219edff67e1301037b9a15d40bdd0192eb13c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-30 17:20:02 +0200

    Create AttributeConfigProvider.php

---
+++ src/AttributeConfigProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bermuda\Config;
+
+use Bermuda\ClassScanner\ClassFinder;
+
+class AttributeConfigProvider
+{
+    public function __construct(
+        private readonly string $directory,
+    ) {
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function __invoke(): array
+    {
+        $data = [];
+        foreach (new ClassFinder()->find($this->directory) as $reflectionClass) {
+            if ($reflectionClass->getAttributes(AsConfig::class)[0] ?? null) {
+                if (!$reflectionClass->hasMethod('__invoke')) {
+                    throw new \RuntimeException('Invalid provider: ' . $reflectionClass->getName());
+                }
+
+                $provider = $reflectionClass->newInstanceWithoutConstructor();
+                $data = array_merge_recursive($data, $provider->__invoke());
+            }
+        }
+
+        return $data;
+    }
+}
